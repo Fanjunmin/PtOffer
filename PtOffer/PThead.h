e5c9f17b6e6162f44b9e85bcb59373b426761340
fanfan <junminfan@126.com>
2018-06-11 22:24:32 +0800

    Add implementations of all functions.

--- PtOffer/PThead.h
+++ PtOffer/PThead.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <bits/stdc++.h>
+#include <regex>
 using namespace std;
 struct TreeNode {
 	int val;
@@ -33,6 +34,7 @@ struct RandomListNode {
 		label(x), next(NULL), random(NULL) {
 	}
 };
+
 /*********************************************************/
 /*------------------------Array数组-----------------------*/
 /**********************************************************/
@@ -42,3 +44,54 @@ bool Find(int target, vector<vector<int> > array);
 
 //旋转数组的最小数字
 int minNumberInRotateArray(vector<int> rotateArray);
+
+//调整数组顺序使奇数位于偶数前面
+void reOrderArray(vector<int> &array);
+
+//数组中出现次数超过一半的数字
+int MoreThanHalfNum_Solution(vector<int> numbers);
+
+//连续子数组的最大和
+int FindGreatestSumOfSubArray(vector<int> array);
+
+//把数组排成最小的数
+string PrintMinNumber(vector<int> numbers);
+
+//数组中的逆序对
+long mergeCount(vector<int>& data, int lo, int hi);
+int InversePairs(vector<int> data);
+
+//数字在排序数组中出现的次数
+int GetNumberOfK(vector<int> data, int k);
+
+//数组中只出现一次的数字
+void FindNumsAppearOnce(vector<int> data, int* num1, int *num2);
+
+//数组中重复的数字
+bool duplicate(int numbers[], int length, int* duplication);
+
+//顺时针打印矩阵
+vector<int> printMatrix(vector<vector<int> > matrix);
+
+//和为S的连续正数序列
+vector<vector<int> > FindContinuousSequence(int sum);
+
+//丑数
+int GetUglyNumber_Solution(int index);
+
+//和为S的两个数字
+vector<int> FindNumbersWithSum(vector<int> array, int sum);
+
+//扑克牌顺子
+bool IsContinuous(vector<int> numbers);
+
+//数据流中的中位数
+void Insert(int num);
+double GetMedian();
+
+//最小的k个数
+vector<int> GetLeastNumbers_Solution(vector<int> input, int k);
+
+/************************************************************/
+/*----------------------------字符串------------------------*/
+/************************************************************/
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 剑指offer题解：
 - 增加了单元测试，使用 [Google Test](https://github.com/google/googletest)。
 - 编译工具VS2017，[配置](https://blog.csdn.net/ruyueyini/article/details/47448211)和[编译部分测试用例](https://blog.csdn.net/halfdream/article/details/30490721)。
+- 头文件<bits/stdc++.h>在VS中的设置见[在VS中使用#include <bits/stdc++.h>的方法](https://blog.csdn.net/dragon60066/article/details/56529077)。
 - 剑指offer在线编程地址：[牛客网](https://www.nowcoder.com/ta/coding-interviews)。
 - 主要文件目录
     1. PtOffer/PThead.h ：函数头文件声明
